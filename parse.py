58f0c8c64764e1d79a19ab31b33408aa8b887660
Zarino Zappia <mail@zarino.co.uk>
2013-06-30 12:03:01 +0100

    frontend now runs parse.py and displays errors

--- parse.py
+++ parse.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 
 import sys
+import xml
 import json
 import dataset
 import plistlib
 
 
+class InvalidArgumentError(Exception):
+    pass
+
+
+class InvalidXMLFileError(Exception):
+    pass
+
+
 def main():
     try:
         if len(sys.argv) != 2:
-            raise ValueError("parse.py takes exactly one argument: the path to an iTunes plist. %s provided." % len(sys.argv[1:]))
+            raise InvalidArgumentError("parse.py takes exactly one argument: the path to an iTunes plist. %s provided." % len(sys.argv[1:]))
         else:
             save(parse(sys.argv[1]))
 
@@ -31,7 +40,12 @@ def main():
 
 
 def parse(filename):
-    pass
+    try:
+        p = plistlib.readPlist(filename)
+    except xml.parsers.expat.ExpatError, e:
+        raise InvalidXMLFileError("Invalid XML file supplied: %s" % filename)
+    except IOError:
+        raise InvalidXMLFileError("Invalid XML file supplied: %s" % filename)
 
 
 def save(library):
